b1ddd2247d0c2f55fb162744f9ecd915e04efdd2
facundotoconas <facutoconas88@gmail.com>
2021-04-19 05:18:58 -0300

    show only required fields

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -27,7 +27,7 @@ RailsAdmin.config do |config|
     dashboard                     # mandatory
     index                         # mandatory
     new
-    export
+    #export
     bulk_delete
     show
     edit
@@ -37,5 +37,13 @@ RailsAdmin.config do |config|
     ## With an audit adapter, you can add:
     # history_index
     # history_show
+    config.model 'Branch' do 
+      fields :id, :name, :address
+      # ...
+    end
+    config.model 'User' do 
+      exclude_fields :created_at, :updated_at, :reset_password_sent_at, :remember_created_at
+      # ...
+    end
   end
 end
