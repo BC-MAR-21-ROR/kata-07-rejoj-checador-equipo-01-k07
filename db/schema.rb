80552944b2c0a96537c7c0a1a4cabf040f135dfd
facundotoconas <facutoconas88@gmail.com>
2021-04-21 02:13:37 -0300

    run migration
    Co-authored-by: marianoalarcon <mariano.alarcon@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_054335) do
+ActiveRecord::Schema.define(version: 2021_04_21_045714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_04_15_054335) do
   create_table "branches", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "log_times", force: :cascade do |t|
+    t.datetime "check_in"
+    t.datetime "check_out"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
