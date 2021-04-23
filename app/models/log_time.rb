925628327beaad5c4904aa4d90207e1128cb94b7
facundotoconas <facutoconas88@gmail.com>
2021-04-23 18:20:02 -0300

    add scope assistence for report of LogTime

--- app/models/log_time.rb
+++ app/models/log_time.rb
@@ -3,4 +3,7 @@
 class LogTime < ApplicationRecord
   belongs_to :user
   belongs_to :branch
+  scope :assistance, -> (date) { where(check_in: date.midnight..date.end_of_day) } # para reporte de asistencia por dia
+  
+  scope :report, -> (date) { where(check_in: date.beginning_of_month..date.end_of_month) }
 end
