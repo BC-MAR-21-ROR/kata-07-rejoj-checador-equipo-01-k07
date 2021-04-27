1c229e3ba94dbdf5f01c200abd3249126fb67136
MIguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2021-04-27 11:38:29 -0500

    Revert "Feature#17 add bootstrap form in views"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: "pages#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
